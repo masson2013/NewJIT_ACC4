ceb1acb890c1dfea1ef39cb4e6a86e624f4f195b
masson2013 <masson.contact@gmail.com>
2016-03-30 14:01:53 -0700

    First Commit of ACC4

---
+++ firmware/AutoBuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Sen Ma
+build-pico-fw.sh m505lx325.fwproj project
+./build-pico-jit-static.sh M505_LX325T_NewJIT_ACC8
+./build-pico-jit-module.sh M505_LX325T_NewJIT_ACC8 acc_vadd.dcp
+./build-pico-jit-module.sh M505_LX325T_NewJIT_ACC8 acc_vmul.dcp
+./build-pico-jit-module.sh M505_LX325T_NewJIT_ACC8 acc_vredu.dcp
